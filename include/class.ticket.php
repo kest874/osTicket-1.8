1b685441ba259ea11597cf224b0e42dd2c0c053d
Jared Hancock <jared@osticket.com>
2015-08-11 08:54:23 -0500

    oops: Log string staff name in event, not AgentName object

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -2075,7 +2075,7 @@ implements RestrictedAccess, Threadable {
                 if ($thisstaff && $thisstaff->getId() == $assignee->getId())
                     $evd['claim'] = true;
                 else
-                    $evd['staff'] = array($assignee->getId(), $assignee->getName());
+                    $evd['staff'] = array($assignee->getId(), (string) $assignee->getName());
             }
         } elseif ($assignee instanceof Team) {
             if ($this->getTeamId() == $assignee->getId()) {
