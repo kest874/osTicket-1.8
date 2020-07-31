d4a55a7c9c5993d728cd12aba2925cf986781f08
Kest874 <ken.stoner@nasg.net>
2020-07-31 12:07:39 -0500

    Add additioanl staff

--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -244,7 +244,7 @@ if($msg) {echo "$.Notification.notify('success','top right', '', '".$msg."');";}
 <?php  
  		$_SESSION['scv19'] = 0;    
  		
- 		if ($thisstaff->staff_id == 0 || $thisstaff->staff_id == 1 || $thisstaff->staff_id == 69) $_SESSION['scv19'] = 1; 
+ 		if ($thisstaff->staff_id == 1 || $thisstaff->staff_id == 6 || $thisstaff->staff_id == 29) $_SESSION['scv19'] = 1; 
 ?>
             <div class="left side-menu">
                 <div class="sidebar-inner slimscrollleft">
