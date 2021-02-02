1c38592089f5781ec90d31b194246d2f89fe5661
Kest874 <ken.stoner@nasg.net>
2021-02-02 08:29:25 -0600

    Embed change

--- include/staff/dashboard.inc.php
+++ include/staff/dashboard.inc.php
@@ -3214,7 +3214,7 @@ Highcharts.chart('associatetrend', {
 	
 	       <div class="mt-3 text-center  powerbi-container">
 	
-	          <iframe  width="500" height="500" src="https://app.powerbi.com/view?r=eyJrIjoiOWQ0MGU4YmEtODAxMy00NDIyLTljYWMtNWY5NWJkNzIwNGYwIiwidCI6ImE0ZjJmOTdiLTcwNWMtNDFlYi1iMjc1LTU5NmU1ZjBhZjBiYiIsImMiOjN9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+	          <iframe  width="500" height="500" src="https://app.powerbi.com/view?r=eyJrIjoiMWQ2NDM1NmEtN2E5My00YmU3LTk2YjItYzY2MDZiOGQ1NGU3IiwidCI6ImE0ZjJmOTdiLTcwNWMtNDFlYi1iMjc1LTU5NmU1ZjBhZjBiYiIsImMiOjN9" frameborder="0" allowFullScreen="true"></iframe>
 	       
 	    
 	       </div>
